458b3f04860c3745d7d12bd61e6b091a50cdee50
sungjiwoon <nebulaisme@naver.com>
2022-12-18 22:47:40 +0900

    221218 stack 공부

---
+++ src/B_1874.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.util.Stack;
+
+public class B_1874 {
+	public void work() throws NumberFormatException, IOException {
+		Stack<Integer> st = new Stack<Integer>();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		Stack<Integer> stack = new Stack<Integer>();
+		int N = Integer.parseInt(br.readLine());
+		int[] nums = new int[N];
+		for (int i = 0; i < N; i++) {
+			nums[i] = Integer.parseInt(br.readLine());
+		}
+		int i = 1;
+		int index = 0;
+		while (i <= N) {
+			stack.push(i);
+			sb.append("+\n");
+			
+			while (!stack.isEmpty() && stack.peek().equals(nums[index])) {
+				index++;
+				stack.pop();
+				sb.append("-\n");
+			}
+			
+			i++;
+		}
+		//System.out.println(sb);
+		if (index != N) {
+			System.out.println("NO");
+		} else {
+			System.out.println(sb);
+		}
+		
+	}
+}
