d7e0981a5b5396a970561ba1685feb0a213bddcb
sungjiwoon <nebulaisme@naver.com>
2023-01-17 00:45:08 +0900

    230117

---
+++ src/b_12_backtracking/B_1182.java
@@ -0,0 +1,58 @@
+package b_12_backtracking;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+/*
+ * 문제 : 부분 수열의 합 
+ * 난이도 : 실버 2
+ * 문제
+	N개의 정수로 이루어진 수열이 있을 때, 크기가 양수인 부분수열 중에서 그 수열의 원소를 다 더한 값이 S가 되는 경우의 수를 구하는 프로그램을 작성하시오.
+	
+	입력
+	첫째 줄에 정수의 개수를 나타내는 N과 정수 S가 주어진다. (1 ≤ N ≤ 20, |S| ≤ 1,000,000) 
+	둘째 줄에 N개의 정수가 빈 칸을 사이에 두고 주어진다. 
+	주어지는 정수의 절댓값은 100,000을 넘지 않는다.
+	
+	출력
+	첫째 줄에 합이 S가 되는 부분수열의 개수를 출력한다.
+	
+	예제 입력 1 
+	5 0
+	-7 -3 -2 5 8
+	예제 출력 1 
+	1
+ */
+public class B_1182 {
+	static int n, s;
+	static int[] map;
+	static boolean[] vis;
+	static int[] arr;
+	static int cnt = 0;
+	private static void func(int idx, int count) {
+		if (idx == n) {
+			if (count==s) cnt++;
+			return;
+		}
+		
+		func(idx+1, count+arr[idx]);
+		func(idx+1, count);
+		
+	}
+	public void work() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		n = Integer.parseInt(st.nextToken());
+		s = Integer.parseInt(st.nextToken());
+		
+		map = new int[n];
+		vis = new boolean[n];
+		arr = new int[n+1];
+		map = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+		func(0,0);
+		if (s == 0) cnt--;
+		System.out.println(cnt);
+	}
+}
--- src/b_12_backtracking/B_1759.java
+++ src/b_12_backtracking/B_1759.java
@@ -1,18 +1,17 @@
+package b_12_backtracking;
+
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.StringReader;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main {
+public class B_1759 {
 	static int l, c;
 	static char[] map;
 	private static void func(int st, String res) {
 		if (res.length()==l) {
+			if (have_aeiou(res)==0 || l-have_aeiou(res) < 2) return;
 			System.out.println(res);
 			return;
 		}
@@ -23,7 +22,16 @@ public class Main {
 			res = res.substring(0,res.length()-1); //다시 원상 복구용.
 		}
 	}
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	private static int have_aeiou(String res) {
+			int cnt=0;
+			if (res.contains("a")) cnt++;
+			if (res.contains("e")) cnt++;
+			if (res.contains("i")) cnt++;
+			if (res.contains("o")) cnt++;
+			if (res.contains("u")) cnt++;
+			return cnt;
+	}
+	public void work() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		l = Integer.parseInt(st.nextToken());
@@ -33,11 +41,8 @@ public class Main {
 		String s = br.readLine();
 		for (int i = 0; i < c; i++)
 			map[i] = s.charAt(i*2);
-		System.out.println(Arrays.toString(map));
 		Arrays.sort(map);
-		System.out.println(Arrays.toString(map));
 		
 		func(0,"");
-		
 	}
 }
