56613d1b2654380ad2402d5b0fc1bf40e845b5e3
sungjiwoon <nebulaisme@naver.com>
2023-01-26 00:11:12 +0900

    230125

--- src/Main.java
+++ src/Main.java
@@ -1,105 +1,52 @@
 import java.io.*;
 import java.util.*;
 
-class Pair {
-	int X, Y;
-	Pair(int X, int Y) {
-		this.X = X;
-		this.Y = Y;
-	}
-}
-
 public class Main {
-	static int n, m;
-	static int[][] map1 = new int[10][10], map2 = new int[10][10];
-	static int[] dx = {-1,0,1,0}, dy = {0,1,0,-1};
-	static ArrayList<Pair> cctv = new ArrayList<>();
-	static int mn = Integer.MAX_VALUE;
-	private static void upd(Pair p, int dir) {
-		dir %= 4; 
-		int x = p.X;
-		int y = p.Y;
-		while (true) {
-			x += dx[dir];
-			y += dy[dir];
-			if (x < 0 || x >= n || y < 0 || y >= m || map2[x][y] == 6) return;
-			if (map2[x][y] != 0) continue;
-			map2[x][y] = 7;
+	static int[][] arr = new int[4][8];
+	static int[] dir12 = {8,8,8,8};
+	private static void func(int index, int dir, int same) {
+		if (index > 3 || index < 0) return;
+		
+		if (index > 0 && !is_samevalue(index-1) && index-1 != same) {
+			func(index-1, dir*(-1), index);			
 		}
+		
+		if (index < 3 && !is_samevalue(index) && index+1 != same) {
+			func(index+1, dir*(-1), index);
+		}
+		rotate(dir, index);
+	}
+	private static void rotate(int dir, int index) {
+		if (dir == -1) {
+			dir12[index]++;
+		} else {
+			dir12[index]--;
+		}
+	}
+	private static boolean is_samevalue(int a) {
+		return (arr[a][(dir12[a]+2)%8] != arr[a+1][(dir12[a+1]+6)%8]) ? false: true;
 	}
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder(); 
-		
-		/* 선언 및 초기화 부분 */
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-	
-		for (int i = 0; i < n; i++) {
-			st = new StringTokenizer(br.readLine(), " ");
-			for (int j = 0; j < m; j++) {
-				map1[i][j] = Integer.parseInt(st.nextToken());
-				if (map1[i][j] > 0 && map1[i][j] < 6) {
-					cctv.add(new Pair(i,j));
-				}
-			}			
+		for (int i = 0; i < 4; i++) {
+			String s = br.readLine();
+			for (int j = 0; j < 8; j++) arr[i][j] = Integer.parseInt(String.valueOf(s.charAt(j)));	
 		}
 		
-		for (int tmp = 0; tmp < Math.pow(4, cctv.size()); tmp++) {
-			for (int i = 0; i < n; i++) 
-				for (int j = 0; j < m; j++)
-					map2[i][j] = map1[i][j]; 
+		int K = Integer.parseInt(br.readLine());
+		for (int k = 0; k < K; k++) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			int index = Integer.parseInt(st.nextToken());
+			int dir = Integer.parseInt(st.nextToken());			
 			
-			int brute = tmp;
-			for (int i = 0; i < cctv.size(); i++) {
-				int dir = brute % 4;
-				brute /= 4;
-				//4진법 구간. 4의 방향의 모든 경우의 수를 만날 수 있음. 
-				
-				Pair p_c = cctv.get(i);
-				int x = p_c.X;
-				int y = p_c.Y;
-				
-				int monitor = map1[x][y];				
-				Pair p = new Pair(x,y);
-				
-				switch(monitor) {
-				case 1 :
-					upd(p,dir);
-					break;
-				case 2 :
-					upd(p,dir);
-					upd(p,dir+2);
-					break;
-				case 3 :
-					upd(p,dir);
-					upd(p,dir+1);
-					break;
-				case 4 :
-					upd(p,dir);
-					upd(p,dir+1);
-					upd(p,dir+2);
-					break;
-				case 5 :
-					upd(p,dir);
-					upd(p,dir+1);
-					upd(p,dir+2);
-					upd(p,dir+3);
-					break;
-				}
-			}
-			int cnt = 0; //사각지대 갯수
-			for (int i = 0; i < n; i++) {
-				for (int j = 0; j < m; j++) {
-					if (map2[i][j] == 0) cnt++;
-				}
-			}
-			mn = Math.min(cnt, mn);
-			
-			
+			func(index-1, dir, index-1);
 		}
-		System.out.println(mn);
+		
+		int count = 0;
+		for (int i = 0; i < 4; i++) {
+			count += Math.pow(2, i)*arr[i][dir12[i]%8];
+		}
+		System.out.println(count);
 		
 	}
 }
