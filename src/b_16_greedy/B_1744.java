d22ab622c44f391cac8b34b4afe7ddaea5b7fd2f
sungjiwoon <nebulaisme@naver.com>
2023-02-09 19:43:07 +0900

    230209

---
+++ src/b_16_greedy/B_1744.java
@@ -0,0 +1,28 @@
+package b_16_greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class B_1744 {
+	public void work() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder(); 
+		
+		/* 선언 및 초기화 부분 */
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[51];
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		
+		long ans = 0;
+		for (int i = 0; i < n-1; i++) {
+			
+		}
+		
+		
+	}
+}
