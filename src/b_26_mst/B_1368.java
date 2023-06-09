c6026e438e3536ffd98d4e8db174614db4552b70
sungjiwoon <nebulaisme@naver.com>
2023-06-09 19:37:47 +0900

    230609 물대기 java

--- src/b_26_mst/B_1368.java
+++ src/b_26_mst/B_1368.java
@@ -2,7 +2,11 @@ package b_26_mst;
 
 import java.io.*;
 import java.util.*;
-
+/*
+ * 물대기
+ * 골드 2
+ * 스스로 물을 채우는 경우라면, 임의의 정점을 새로 만들어 측정한다. 
+ */
 public class B_1368 {
 
 	public void work() throws Exception {
@@ -13,19 +17,13 @@ public class B_1368 {
 		//굳이 다른 논과 연결할 필요 없고, 논에 물이 있으면 됨!!!
 		
 		int[] cost = new int[n+1];
-		int min_index = 0;
-		int min = Integer.MAX_VALUE;
 		for (int i = 1; i <= n; i++) {
 			cost[i] = Integer.parseInt(br.readLine());
-			if (min > cost[i]) {
-				min = cost[i];
-				min_index = i;
-			}
 		}
 		
 		
 		HashMap<Integer, ArrayList<int[]>> hm = new HashMap<>();
-		for (int i = 1; i <= n; i++) {
+		for (int i = 1; i <= n+1; i++) {
 			hm.put(i, new ArrayList<int[]>());
 		}
 		
@@ -37,8 +35,17 @@ public class B_1368 {
 				int[] tmp = {k,v};
 				list.add(tmp);
 			}
+			int[] tmp = {n+1, cost[i]};
+			list.add(tmp);
 			hm.put(i, list);
 		}
+		
+		ArrayList<int[]> solo = hm.get(n+1); //혼자 물을 덧대는 곳. 
+		for (int i = 1; i <= n; i++) {
+			int[] tmp = {i, cost[i]};
+			solo.add(tmp);
+		}
+		hm.put(n+1, solo);
 		
 		//우선순위 큐 : 최솟값 정렬하는. 
 		PriorityQueue<int[]> qu = new PriorityQueue<>(new Comparator<int[]>() {
@@ -50,25 +57,23 @@ public class B_1368 {
 		});
 		
 		
-		ArrayList<int[]> list = hm.get(min_index);
+		ArrayList<int[]> list = hm.get(n+1);
 		for (int[] l : list) {
 			qu.add(l);
 		}
 		
 		int cnt = 1;
-		int ans = cost[min_index];
-		boolean[] vis = new boolean[n+1];
-		vis[min_index] = true;
+		long ans = 0;
+		boolean[] vis = new boolean[n+2];
+		vis[n+1] = true;
 		
-		while (cnt < n) {
+		while (cnt <= n) {
 			int[] q = qu.poll();
 			while (vis[q[0]]) q = qu.poll();
-			
-			if (q[1] < cost[q[0]]) {
-				ans += q[1];
-			}
+
 			vis[q[0]] = true;
 			cnt++;
+			ans += q[1];
 			list = hm.get(q[0]);
 			for (int[] l : list) {
 				if (vis[l[0]]) continue;
