9b3105af3790dad4170798beee1cf8dff31ae4a5
sungjiwoon <nebulaisme@naver.com>
2023-06-12 16:05:33 +0900

    230612 java

--- src/b_15_dp/B_12852.java
+++ src/b_15_dp/B_12852.java
@@ -15,11 +15,11 @@ public class B_12852 {
 		for (int i = 2; i <= n; i++) { //i를 1로 만들기 위한 필요한 연산 사용 횟수의 최솟값. 
 			dp[i] = dp[i-1]+1; 
 			pre[i] = i-1;
-			if (i % 2 == 0) {
+			if (i % 2 == 0 && dp[i / 2] + 1 < dp[i]) {
 				dp[i] = Math.min(dp[i], dp[i/2]+1);
 				pre[i] = i/2;
 			} 			
-			if (i % 3 == 0) {
+			if (i % 3 == 0 && dp[i / 3] + 1 < dp[i]) {
 				dp[i] = Math.min(dp[i], dp[i/3]+1);
 				pre[i] = i/3;
 			} 
