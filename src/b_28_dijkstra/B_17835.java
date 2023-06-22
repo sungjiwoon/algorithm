f5c44d00c1680b5c73928be3b01a8e7191140e20
sungjiwoon <nebulaisme@naver.com>
2023-06-22 15:01:10 +0900

    230622 다익스트라

--- src/b_28_dijkstra/B_17835.java
+++ src/b_28_dijkstra/B_17835.java
@@ -3,60 +3,23 @@ import java.io.*;
 import java.util.*;
 
 //다시 풀기 
-public class B_17835 {
-	static ArrayList<Integer> iv = new ArrayList<>();
-	static int n;
-	static ArrayList<int[]>[] adj;
-	private static int[] solve(int st) {
-		
-		int[] d = new int[n+1];
-		PriorityQueue<int[]> qu = new PriorityQueue<int[]>(new Comparator<int[]>() {
-			@Override
-			public int compare(int[] o1, int[] o2) {
-				return o1[1]- o2[1]; //거리 순 오름차순. 
-			}
-		});
-		
-		Arrays.fill(d, Integer.MAX_VALUE/1000);
-		d[st] = 0;
-		
-		int[] tmp = {st, d[st]};
-		qu.add(tmp);
-		
-		while (!qu.isEmpty()) {
-			int[] q = qu.poll();
-			if (d[q[0]] != q[1]) continue;
-			
-			for (int[] nxt : adj[q[0]]) {
-				if (d[nxt[0]] < d[q[0]] + nxt[1]) continue;
-				
-				d[nxt[0]] = d[q[0]] + nxt[1];
-				int[] tmp2 = {nxt[0], d[nxt[0]]};
-				qu.add(tmp2);
-			}
-		}
-		
-		int min_index = -1;
-		int min = Integer.MAX_VALUE;
-		for (int kk : iv) {
-			if (min > d[kk]) {
-				min = d[kk];
-				min_index = kk;
-			}
-		}
-		int[] res = {min_index, min};
-		return res;
-		
-	}
+
+/* 면접보는 승범이네 (골드 2) 
+ * 다익스트라 
+ * 면접장->집 까지 중 최소 거리를 구하는 것이 포인트이다. 
+ * 모든 도시를 갈 수 있는것이 확정이므로. 
+ * 다만 반대의 거리이므로 입력받을 때 거꾸로 해줘야함...
+ */
+public class B_17835 {	
 	public void work() throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		
-		n = Integer.parseInt(st.nextToken());
+		int n = Integer.parseInt(st.nextToken());
 		int m = Integer.parseInt(st.nextToken());
 		int k = Integer.parseInt(st.nextToken());		
 		
-		adj = new ArrayList[n+1];
+		ArrayList<int[]>[] adj = new ArrayList[n+1];
 		for (int i = 1; i <= n; i++) {
 			adj[i] = new ArrayList<>();
 		}
@@ -66,30 +29,47 @@ public class B_17835 {
 			int u = Integer.parseInt(st.nextToken());
 			int v = Integer.parseInt(st.nextToken());
 			int c = Integer.parseInt(st.nextToken());
-			int[] tmp = {v,c};
-			adj[u].add(tmp);			
-		}
+			int[] tmp = {u,c}; //면접장에서 찾을 거니. 반대루트를 따라야함!! 
+			adj[v].add(tmp);			
+		}	
 		
-		int[] res = new int[n+1]; //면접장을 담는 배열. 
-		int[] d = new int[n+1]; //면접장과의 거리를 담는 배열. 
-		Arrays.fill(d, -1);
+		
+		//면접장에서 가장 가까운 도시를 찾아 return;; 
+		PriorityQueue<long[]> qu = new PriorityQueue<>(new Comparator<long[]>() {
+			@Override
+			public int compare(long[] o1, long[] o2) {
+				return (int)(o1[1] - o2[1]);
+			}
+		});
+		
+		long[] d = new long[n+1];
+		Arrays.fill(d, 100_000_000_000L);
+		
 		st = new StringTokenizer(br.readLine(), " ");
 		for (int i = 0; i < k; i++) {
 			int kk = Integer.parseInt(st.nextToken());
-			iv.add(kk);
-			
-			res[kk] = kk;
 			d[kk] = 0;
+			long[] tmp = {kk, d[kk]};
+			qu.add(tmp);
 		}
 		
-		int max = 0;
-		for (int i = 1; i <= n; i++) {
+		while (!qu.isEmpty()) {
+			long[] q = qu.poll();
 			
-			if (d[i] == 0) continue;
-			int[] value = solve(i);
-			res[i] = value[0];
-			d[i] = value[1];
-			//System.out.println(i + ": " + res[i] + " " + d[i]);
+			if (d[(int)q[0]] != q[1]) continue;
+			
+			for (int[] nxt : adj[(int)q[0]]) {
+				if (d[nxt[0]] < q[1] + nxt[1]) continue;
+				d[nxt[0]] = q[1] + nxt[1];
+				long[] tmp2 = {nxt[0], d[nxt[0]]};
+				qu.add(tmp2);
+			}
+			
+		}
+		
+		int max = 1;
+		for (int i = 1; i <= n; i++) {
+			//System.out.println(i + " : " +d[i] + " ");
 			if (d[max] < d[i]) {
 				max = i;
 			}
@@ -97,7 +77,6 @@ public class B_17835 {
 		
 		System.out.println(max);
 		System.out.println(d[max]);
-		
 		
 		
 	}
