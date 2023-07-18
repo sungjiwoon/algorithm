09f1b206a1ab87331dec731ff422b8f97c7306eb
sungjiwoon <nebulaisme@naver.com>
2023-07-18 17:57:48 +0900

    햣

--- src/Main.java
+++ src/Main.java
@@ -3,38 +3,9 @@ import java.util.*;
 import java.util.Map.Entry;
 
 public class Main {	
-	static int ans = 0, n, m;
-	static int[] seq;
-	private static void func(int depth, int sum, int op1, int op2) {
-		if (depth == m) {
-			ans = Math.min(ans, sum);
-			return;
-		}
-		
-		//op1을 선택할 경우 
-		func(depth+1, sum+Math.abs(seq[depth]-op1), seq[depth], op2);
-		
-		func(depth+1, sum+Math.abs(seq[depth]-op2), op1, seq[depth]);
-		
-	}
+
 	public static void main(String[] args) throws Exception {	
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		/* 변수 초기화 과정 */
-		n = Integer.parseInt(br.readLine()); //벽장의 갯수
-		
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		int op1 = Integer.parseInt(st.nextToken()); //열려있는 벽장문1
-		int op2 = Integer.parseInt(st.nextToken()); //열려있는 벽장문 2
-		
-		m = Integer.parseInt(br.readLine()); //순서의 길이
-		seq = new int[m]; //벽장의 순서대로 담는 배열. 
-		for (int i = 0; i < m; i++) seq[i] = Integer.parseInt(br.readLine());
-		
-		/* 계산 시작 */
-		
-		int ans = 0;
-		
-		System.out.println(ans);
+
 	
 	}
 
---
+++ src/a_mac_test/Sample.java
@@ -0,0 +1,7 @@
+package a_mac_test;
+
+public class Sample {
+    public static void main(String[] args) {
+        System.out.println("hello world"); //테스트 파일
+    }
+}
