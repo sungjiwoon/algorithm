0cd0771bd2b2fb0aa8ca3f6d0a749cd0d9d9e69d
sungjiwoon <nebulaisme@naver.com>
2024-01-18 18:41:02 +0900

    240118 백준 포도주 시식 (실버 1) 다이나믹 프로그래밍 DP

--- src/b_15_dp/B_2156.java
+++ src/b_15_dp/B_2156.java
@@ -4,32 +4,51 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/**
+ * 240118 ë°±ì¤€ í¬ë„ì£¼ ì‹œì‹ (ì‹¤ë²„ 1)
+ * DP
+ */
 public class B_2156 {
-	public void work() throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuffer sb = new StringBuffer();
-		
-		int n = Integer.parseInt(br.readLine());
-		
-		int[] map = new int[n+1];
-		long[] dp = new long[n+1];
-		
-		for (int i = 1; i <= n; i++) map[i] = Integer.parseInt(br.readLine());
-		
-		//3ÀÜ ¿¬¼Ó ¤¤¤¤
-		// 6 10 9 8 = 33
-		
-		// ÀüÀü²¬ ¾È¸¶¼ÌÀ» ¶§ÀÇ ÃÖ´ñ°ª. 
-		
-		dp[1] = map[1];
-		if (n >= 2) {
-			dp[2] = map[1]+map[2];
+	int n;
+	int[] info;
+
+	private int solve() {
+		int[] dp = new int[10001];
+
+		dp[1] = info[1];
+		if (n >= 2) dp[2] = info[2] + info[1];
+		if (n >= 3) dp[3] = Math.max(Math.max(info[1], info[2]) + info[3], dp[2]);
+
+		for (int i = 4; i <= n; i++) {
+			dp[i] = Math.max(dp[i-3] + info[i-1], dp[i-2]) + info[i];
+			dp[i] = Math.max(dp[i-1], dp[i]);
 		}
-		
-		for (int i = 3; i <= n; i++) {
-			dp[i] = Math.max(dp[i-3]+map[i-1], dp[i-2]) + map[i];
-			System.out.println(i + " " + dp[i]);
-		}
-		System.out.println(Math.max(dp[n], dp[n-1]));
+
+		return dp[n];
 	}
+
+
+	public static void main(String[] args) {
+		B_2156 m = new B_2156();
+
+//		Main m = new Main();
+
+		m.init();
+		System.out.println(m.solve());
+
+	}
+
+	private void init() {
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+			n = Integer.parseInt(br.readLine());
+			info = new int[n + 1];
+			for (int i = 1; i <= n; i++) {
+				info[i] = Integer.parseInt(br.readLine());
+			}
+
+		} catch (Exception e) {}
+	}
+
 }
