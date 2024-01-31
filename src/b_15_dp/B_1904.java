f6e17348e45a89c3cd1b1affa7083ad0330df3a1
sungjiwoon <nebulaisme@naver.com>
2024-02-01 01:02:41 +0900

    240201 DP 실버 3 : 01 타일

--- src/b_15_dp/B_1904.java
+++ src/b_15_dp/B_1904.java
@@ -5,28 +5,27 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class B_1904 {
-	public void work() throws NumberFormatException, IOException {
+
+	public static void main(String[] args) throws Exception {
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuffer sb = new StringBuffer();
-		
 		int n = Integer.parseInt(br.readLine());
-		
-		int[] dp = new int[n+1];
-		
-		dp[1] = 1; //1
-		if (n >= 2) dp[2] = 2; //00, 11
-		
-//		dp[3] = 3; //001, 100, 111
-//		dp[4] = 5;//0000, 1001, 0011, 1100, 1111
-//		
-//		dp[5] = 8;//00111, 11100, 10011, 11001, 00001, 10000, 00100, 11111
-		//짝수일 땐, 0으로 이루어진 수가 하나 추가되고,
-		//전체 수에선 +1, 
-		
+		int[] dp = new int[1000001];
+
+		// 현재 길이가 짝수 일 경우 전 단계 가짓수 그대로 가져감. 1 -> 11,
+
+		// 1 / 00 11 / 001 100 111
+		// 0011 1001 1111 0000 1100 (5)
+		// 00111 10011 11111 00001 11001 00100 10000 11100 (5 + 3)
+
+		dp[1] = 1;
+		dp[2] = 2;
+
 		for (int i = 3; i <= n; i++) {
 			dp[i] = dp[i-1] + dp[i-2];
 			dp[i] %= 15746;
 		}
+
 		System.out.println(dp[n]);
 	}
 }
