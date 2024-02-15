b3bd9922fb29c284522381602fb00b776004ab81
sungjiwoon <nebulaisme@naver.com>
2024-02-15 23:47:35 +0900

    240215 이동하기 실버 2 DP

--- src/hell_study/B_11048.java
+++ src/hell_study/B_11048.java
@@ -1,11 +1,34 @@
 package hell_study;
 import java.util.*;
 import java.io.*;
+
 /** 240215 이동하기 실버 2 DP */
+
 public class B_11048 {
     int n, m;
-    public static void main(String[] args) {
+    int[][] map;
+    private int solve() {
+        int[] dx = {-1, 0, -1}, dy = {0, -1, -1};
+        int[][] dp = new int[n][m];
 
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                for (int k = 0; k < 3; k++) {
+                    int bx = i + dx[k], by = j + dy[k];
+                    if (bx < 0 || by < 0 || bx >= n || by >= m) continue;
+                    dp[i][j] = Math.max(dp[bx][by], dp[i][j]);
+                }
+                dp[i][j] += map[i][j];
+            }
+        }
+
+        return dp[n-1][m-1];
+    }
+    public static void main(String[] args) {
+        B_11048 b = new B_11048();
+//        Main b = new Main();
+        b.input();
+        System.out.println(b.solve());
     }
     private void input() {
         try {
@@ -13,6 +36,10 @@ public class B_11048 {
             int[] tmp = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
             n = tmp[0];
             m = tmp[1];
+            map = new int[n][m];
+            for (int i = 0; i < n; i++) {
+                map[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            }
         } catch(Exception e) {}
     }
 }
