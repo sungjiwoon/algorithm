4f5da223f3e384ee71b186b9ca3f625b5907d006
sungjiwoon <nebulaisme@naver.com>
2024-02-18 23:26:12 +0900

    240218 Four Squares (실버 3) DP

--- src/hell_study/B_17626.java
+++ src/hell_study/B_17626.java
@@ -3,12 +3,19 @@ package hell_study;
 import java.util.*;
 import java.io.*;
 
+/** 240218 Four Squares (실버 3) */
 public class B_17626 {
 
     public static void main(String[] args) throws Exception {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 3;
+        dp[4] = 1;
 
         // 26 => 25 + 1 (2)
 
