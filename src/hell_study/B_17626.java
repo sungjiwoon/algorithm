c3d10d9273fd79c3285b10ec6eac0cd810642e7e
sungjiwoon <nebulaisme@naver.com>
2024-02-19 01:10:33 +0900

    240218 Four Squares (실버 3) DP

--- src/hell_study/B_17626.java
+++ src/hell_study/B_17626.java
@@ -5,35 +5,25 @@ import java.io.*;
 
 /** 240218 Four Squares (실버 3) */
 public class B_17626 {
-
     public static void main(String[] args) throws Exception {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-
-        Map<Integer, Boolean> map = new HashMap<>();
-
-        for (int i = 1; i * i <= 50000; i++) {
-            map.put(i * i, true);
-        }
 
         int[] dp = new int[50001];
         dp[1] = 1;
         dp[2] = 2;
         dp[3] = 3;
 
-        int near = 1;
         for (int i = 4; i <= n; i++) {
-            if (map.containsKey(i)) {
-                near = i;
+            int min = Integer.MAX_VALUE;
+            for (int j = 1; j * j <= i; j++) {
+                min = Math.min(min, dp[i - j*j]);
             }
-            dp[i] = 1 + dp[i - near];
-//            System.out.println(i + ": " + dp[i]);
+            dp[i] = min + 1;
         }
 
         System.out.println(dp[n]);
-
-
 
         // 26 => 25 + 1 (2)
 
