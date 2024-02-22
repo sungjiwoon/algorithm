99864c444fb7e01ff15835faaa3e22ae49a9e940
sungjiwoon <nebulaisme@naver.com>
2024-02-22 13:53:45 +0900

    240222 백준 오르막수 DP

--- src/b_15_dp/B_11057.java
+++ src/b_15_dp/B_11057.java
@@ -11,8 +11,8 @@ public class B_11057 {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        int[][] dp = new int[1001][10];
-        Arrays.fill(dp[1], 1);
+        int[][] dp = new int[1001][10]; // dp[i][j] -> 수의 길의 i의 첫째 자리수가 j인 경우의 갯수
+        Arrays.fill(dp[1], 1); // n = 1은 무조건 전부 1개이다.
 
         for (int i = 2; i <= n; i++) {
             dp[i][9] = 1;
