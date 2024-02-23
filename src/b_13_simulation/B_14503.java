ef67485675a2436bb775c33078e1cbaee3f3ff7e
sungjiwoon <nebulaisme@naver.com>
2024-02-23 15:54:25 +0900

    240223 백준 14503 로봇청소기 골5 구현

--- src/b_13_simulation/B_14503.java
+++ src/b_13_simulation/B_14503.java
@@ -13,18 +13,72 @@ public class B_14503 {
 	static int[] dr = {-1, 0, 1, 0}, dc = {0, 1, 0, -1};
 	static final int EMPTY = 0, WALL = 1, CLEAN = 2;
 
-	class Pair {
+	static class Pair {
 		int r, c;
 		Pair(int r, int c) {
 			this.r = r;
 			this.c = c;
 		}
 	}
+	private static boolean isRange(int nr, int nc) {
+		return (nr >= 0 && nc >= 0 && nr < n && nc < m);
+	}
 	private static int solve() {
-		int res = 0;
-
 		Queue<Pair> qu = new LinkedList<>();
+		qu.add(new Pair(r, c));
+		while (!qu.isEmpty()) {
+			Pair p = qu.poll();
+			map[p.r][p.c] = CLEAN; // 1번 - 청소
 
+			boolean isClean = true;
+			for (int k = 0; k < 4; k++) {
+				int nr = p.r + dr[k];
+				int nc = p.c + dc[k];
+				if (!isRange(nr, nc)) continue;
+				if (map[nr][nc] == EMPTY) {
+					// 3번 청소되지 않는 빈칸이 있는 경우
+					isClean = false;
+					break;
+				}
+			}
+
+			if (isClean) {
+				// 2번 주변 4칸 중 청소되지 않은 빈칸이 없는 경우
+				// 2-1 바라보는 방향 유지한채, 한칸 후진할 수 있다면 후진.
+				int backD = (d + 4 - 2) % 4;
+				int nr = p.r + dr[backD];
+				int nc = p.c + dc[backD];
+				if (isRange(nr, nc) && map[nr][nc] != WALL) {
+					qu.add(new Pair(nr, nc));
+					continue;
+				}
+				// 2-2 바라보는 방향의 뒤쪽 칸이 벽이라 후진할 수 없다면 STOP
+				if (isRange(nr, nc) && map[nr][nc] == WALL) {
+					break; // 종료 지점
+				}
+			} else { // 3번 청소되지 않는 빈칸이 있는 경우
+				// 3-1 반시계 회전.
+				d = (d + 4 - 1) % 4;
+				// 3-2 바라보는 방향으로 앞쪽 칸이 청소되지 않은 빈칸인 경우 한칸 전진한다.
+				int nr = p.r + dr[d];
+				int nc = p.c + dc[d];
+				if (isRange(nr, nc) && map[nr][nc] == EMPTY) {
+					qu.add(new Pair(nr, nc));
+				} else {
+					// 원래 있던거 다시 넣음.
+					qu.add(p);
+				}
+			}
+		}
+
+		int res = 0;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				if (map[i][j] == CLEAN) {
+					res++;
+				}
+			}
+		}
 		return res;
 	}
 
