f9d83627528d9befac98f84e543b6ef63f25a26e
sungjiwoon <nebulaisme@naver.com>
2024-10-06 23:35:47 +0900

    241006

--- src/b_202410/B_14442.java
+++ src/b_202410/B_14442.java
@@ -1,40 +1,32 @@
 package b_202410;
 
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 /**
- * 벽 부수고 이동하기 (골3)
- * BFS 응용
+ * 241006
+ * 벽 부수고 이동하기 3 (골3)
+ * BFS 응용 > 시간 초과
  */
 
 public class B_14442 {
-
     int[][] map = new int[1001][1001];
     int[] dx = {-1, 0, 1, 0}, dy = {0, -1, 0, 1};
     int n, m, k;
 
     public void solve() {
-        int[][][] dist = new int[1001][1001][2];
-        // dist[x][y][0] - 벽을 하나도 부수지 않고, (x,y)까지 오는데 비용
-        // dist[x][y][1] - 벽을 하나만 부수고, (x,y) 까지 오는데 비용. (x,y)가 벽이라서 부수는 비용도 포함
-
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                dist[i][j][0] = dist[i][j][1] = -1;
-            }
-        }
+        int[][][] dist = new int[1001][1001][12];
+        // dist[x][y][k] - 벽을 k번 부수고, (x,y) 까지 오는데 비용. (x,y)가 벽이라서 부수는 비용도 포함
 
         Queue<int[]> qu = new LinkedList<>();
         qu.add(new int[]{1, 1, 0});
-        dist[1][1][0] = dist[1][1][1] = 1;
+        dist[1][1][0] = 1;
 
         int res = -1;
-
         while (!qu.isEmpty()) {
             int[] q = qu.poll();
             int x = q[0], y = q[1], broken = q[2];
-
+            //System.out.println(String.format("dist[%d][%d][%d] = %d", x, y, broken, dist[x][y][broken]));
             if (x == n && y == m) {
                 res = dist[x][y][broken];
                 break;
@@ -45,17 +37,16 @@ public class B_14442 {
                 int nx = x + dx[d], ny = y + dy[d];
                 if (nx <= 0 || nx > n || ny <= 0 || ny > m) continue;
 
-                if (map[nx][ny] == 0 && dist[nx][ny][broken] == -1) {
-                    qu.add(new int[]{nx, ny, broken});
-                    dist[nx][ny][broken] = nextDist;
+                // 만약 nx, ny가 벽이라면, 넣어보자. // k번까지는 부술 수 있음.
+                int wb = broken;
+                if (map[nx][ny] == 1) {
+                    //System.out.println(String.format("map[%d][%d] = 1, broken = %d", nx, ny, broken+1));
+                    wb++;
                 }
 
-                // nx, ny가 벽인 경우 > 부심 (단 broken = 0)
-                if (map[nx][ny] == 1 && broken == 0 && dist[nx][ny][broken] == -1) {
-                    qu.add(new int[]{nx, ny, 1});
-                    dist[nx][ny][1] = nextDist;
-                }
-
+                if (wb > k || dist[nx][ny][wb] > 0) continue;
+                dist[nx][ny][wb] = nextDist;
+                qu.add(new int[]{nx, ny, wb});
             }
         }
 
@@ -75,7 +66,7 @@ public class B_14442 {
                 .mapToInt(Integer::parseInt).toArray();
         n = arr[0];
         m = arr[1];
-//        k = arr[2];
+        k = arr[2];
 
         for (int i = 1; i <= n; i++) {
             String st = br.readLine();
