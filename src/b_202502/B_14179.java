10453d6976f29006e3f2c5bb4f52c774d9f41d77
sungjiwoon <nebulaisme@naver.com>
2025-02-19 23:47:56 +0900

    250219

---
+++ src/b_202502/B_14179.java
@@ -0,0 +1,53 @@
+package b_202502;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class B_14179 {
+
+    int h, w;
+    int[] blocks;
+
+    private void solve() {
+        Stack<Integer> stack = new Stack<>();
+        int ans = 0;
+
+        // 가로로 접근
+        // 가로 기준으로 값의 사이에 있으면 더해준다?
+        for (int i = h; i >= 0; i--) {
+            stack.clear();
+            for (int j = 0; j < w; j++) {
+                if (blocks[j] >= i) {
+                    if (stack.isEmpty()) {
+                        stack.push(j);
+                    } else {
+                        int pre = stack.pop();
+                        ans += (j - pre - 1);
+                        stack.push(j);
+                    }
+                }
+            }
+        }
+        System.out.println(ans);
+
+    }
+
+    private void init() {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            h = Integer.parseInt(st.nextToken());
+            w = Integer.parseInt(st.nextToken());
+            blocks = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            solve();
+
+
+        } catch (Exception e) {}
+    }
+
+    public static void main(String[] args) {
+        B_14179 b = new B_14179();
+        b.init();
+    }
+}
